4c190ac8d78d1c5acfe036be47812997d6613168
sagarikaraina <112631849+sagarikaraina@users.noreply.github.com>
2024-01-20 14:56:00 +0530

    Update Music store data analysis project.sql

--- Music store data analysis project.sql
+++ Music store data analysis project.sql
@@ -8,29 +8,31 @@ group by billing_country
 order by count desc;
 
 # 3.What are top 3 values of total invoices
-select total from invoice
+select round(total,2) as total from invoice
 order by total desc
 limit 3;
 
 # 4.Which city has the best customers?We would like to throw a promotional music festival in the city we made the most money.
 # Write a query that returns one city that has the highest sum of totals.Return both the city name and sum of all invoice totals
-select billing_city,sum(total) as invoice_total from invoice 
+select billing_city,round(sum(total),2) as invoice_total from invoice 
 group by billing_city
-order by invoice_total desc;
+order by invoice_total desc
+limit 1;
 
 # 5.Who is the best customer?The customer who has spent the most money will be declared the best customer.Write a query that returns the person who has
 # spent the most money
-
--- cte used as first_name and last_name are non-aggregated fields
 with cte as(SELECT cust.customer_id, sum(inv.total) as totals FROM customer cust
 left join invoice inv
 on cust.customer_id=inv.customer_id
 group by cust.customer_id
 order by totals desc limit 1
 )
-select cte.customer_id,cust.first_name,cust.last_name,round(cte.totals,2) from cte join customer cust
+select cte.customer_id,cust.first_name,cust.last_name,round(cte.totals,2) total_spending from cte join customer cust
 on cte.customer_id=cust.customer_id;
  
+ 
+ -- cte used as first_name and last_name are non-aggregated fields
+
  # 6.Write the query to return email,first_name,last_name and genre of all rock music listeners ,return list ordered alphabetically by email
  select distinct(email),cust.first_name,cust.last_name,g.name from customer cust
  join invoice i on cust.customer_id=i.customer_id
@@ -49,16 +51,16 @@ on cte.customer_id=cust.customer_id;
  where g.name='Rock'
  group by art.artist_id
  )
- select cte1.artist_id,cte1.total_track_count,art.name from cte1
+ select cte1.artist_id,art.name,cte1.total_track_count from cte1
  join artist art on art.artist_id=cte1.artist_id
 order by total_track_count desc limit 10;
    
 # 8. Return all track names that have song length greater than average song length.Return the name and milliseconds for each track,order by song length with
 # the longest song listed first
--- subquery used
 SELECT name,milliseconds FROM music_store.track 
 where milliseconds > (select avg(milliseconds) from track)
 order by milliseconds desc;
+
 
 # 9.Find how much amount spent by each customer on artists?Write a query to return customer_name,artist_name and total_spent
  with best_selling_artist as
@@ -69,9 +71,10 @@ order by milliseconds desc;
  join artist art on art.artist_id=a.artist_id
  group by 1,2
  order by 3 desc
+ limit 1
  
  )
- select distinct c.first_name,c.last_name,bsa.name,bsa.totalspent 
+ select c.customer_id,c.first_name,c.last_name,bsa.name,round(sum(l.quantity*l.unit_price),2) as totalspent
  from customer c
  join invoice i on c.customer_id=i.customer_id
  join invoice_line l on i.invoice_id=l.invoice_id
@@ -79,19 +82,22 @@ order by milliseconds desc;
  join album a on a.album_id=t.album_id
  join best_selling_artist bsa on bsa.artist_id=a.artist_id
  group by 1,2,3,4
- order by 4 desc;
+ order by 5 desc;
 
 # 10.Find out the most popular music genre for each country.Most popular genre is defined by the genre with highest amount of purchases.
 # Write a query that returns each country along with top genre.For countries where maximum number of purchases is shared return all genres
 with most_popular_genre as(
-select c.country,count(il.quantity) as purchases,row_number() over(partition by c.country order by count(il.quantity) desc)
+select c.country as country,count(il.quantity) as purchases,g.genre_id as genre_id,g.name as genre_name,
+row_number() over(partition by c.country order by count(il.quantity) desc) as rn
 from track t
 join genre g on g.genre_id=t.genre_id 
 join invoice_line il on il.track_id=t.track_id
 join invoice i on i.invoice_id=il.invoice_id 
 join customer c on c.customer_id=i.customer_id
-group by c.country)
-select * from most_popular_genre
-order by 2 desc,1 asc;
+group by country,genre_id,genre_name
+order by country,purchases desc
+)
+select country,purchases,genre_id,genre_name from most_popular_genre
+where rn<=1;
 
 
